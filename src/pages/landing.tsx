04330bcce4ae2935a20ce123a7628c80b3901b5e
LaunchThatBrand <info@launchthatbrand.com>
2023-10-10 14:28:45 -0400

    lint fix 3

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -540,8 +540,8 @@ export default function Home() {
                         Message
                       </label>
                       <textarea
-                        rows="4"
-                        cols="80"
+                        rows={4}
+                        cols={80}
                         className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
                         placeholder="Type a message..."
                       />
