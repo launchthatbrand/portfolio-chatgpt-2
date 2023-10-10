d19941813995991bf84b29e94965790e4dc29a8a
LaunchThatBrand <info@launchthatbrand.com>
2023-10-09 23:20:53 -0400

    added creativetim pages and styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "~/styles/globals.css";
+import "~/styles/tailwind.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
