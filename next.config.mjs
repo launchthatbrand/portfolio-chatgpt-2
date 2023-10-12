40c0c1e937f1234f06a5ed43e126e8b5f69334a6
LaunchThatBrand <info@launchthatbrand.com>
2023-10-12 13:06:19 -0400

    added sanity/lib files

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,20 @@ const config = {
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
+  images: {
+    remotePatterns: [
+      { hostname: "cdn.sanity.io" },
+      { hostname: "source.unsplash.com" },
+    ],
+  },
+  typescript: {
+    // Set this to false if you want production builds to abort if there's type errors
+    ignoreBuildErrors: process.env.VERCEL_ENV === "production",
+  },
+  eslint: {
+    /// Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
